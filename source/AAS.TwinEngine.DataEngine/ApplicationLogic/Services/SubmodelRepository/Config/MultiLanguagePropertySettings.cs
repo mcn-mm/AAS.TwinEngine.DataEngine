5ec0f36a8eac7c49a225aea2b806bc9cf8ff51b5
Hardi Shah <hsh@mm-software.com>
2026-02-23 18:09:58 +0530

    Enhance MultiLanguageProperty : Add default language support (#50)

---
+++ source/AAS.TwinEngine.DataEngine/ApplicationLogic/Services/SubmodelRepository/Config/MultiLanguagePropertySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace AAS.TwinEngine.DataEngine.ApplicationLogic.Services.SubmodelRepository.Config;
+
+public class MultiLanguagePropertySettings
+{
+    public const string Section = "MultiLanguageProperty";
+
+    public IList<string>? DefaultLanguages { get; init; }
+}
